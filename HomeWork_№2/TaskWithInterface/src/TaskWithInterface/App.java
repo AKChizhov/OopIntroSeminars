b89d3e7ed3bce5a9dd44e7339da732da64165261
AKChizhov <ChizhovAK2007@yandex.ru>
2023-03-09 00:44:24 +0300

    Добавлены цвета шрифта в ДЗ к семинару №2

--- HomeWork_№2/TaskWithInterface/src/TaskWithInterface/App.java
+++ HomeWork_№2/TaskWithInterface/src/TaskWithInterface/App.java
@@ -3,14 +3,21 @@ package TaskWithInterface;
 import TaskWithInterface.Heroes.Bridge;
 import TaskWithInterface.Heroes.Orc;
 import TaskWithInterface.Heroes.WoodBilding;
-import java.util.Random;
 
 public class App  {
+    static String RED =TaskWithInterface.ColorLib.ANSI_RED;
+    static String YELLOW =TaskWithInterface.ColorLib.ANSI_YELLOW ;
+    static String GREEN =TaskWithInterface.ColorLib.ANSI_GREEN ;
+    static String RESET =TaskWithInterface.ColorLib.ANSI_RESET ;
+    
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("\n\tИмеются : стальной мост, Орк, деревянное строение");
+        
+
+
+        System.out.println(RED+"\n\tИмеются : стальной мост, Орк, деревянное строение"+RESET);
        
         Bridge bridge = new Bridge(Math.random()*100);
-        //Bridge bridge = new Bridge(80);
         Orc orc = new Orc(Math.random()*100, Math.random()*100);
         WoodBilding woodBilding = new WoodBilding(Math.random()*100);
         

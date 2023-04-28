30843b8fbdce8ee6f621c081a73178acc5f7288d
AKChizhov <ChizhovAK2007@yandex.ru>
2023-04-28 17:42:06 +0300

    Создана база данных оперативной памяти компьютера

---
+++ Subtotal_Certification_JAVA/TaskWithComparison/src/TaskWithComparison/BrandCompuetrs.java
@@ -0,0 +1,5 @@
+package TaskWithComparison;
+
+public enum BrandCompuetrs {
+    Gigabyte, Silicon_Power, Crucial, Patriot, Samsung, Apacer, Transcend, AMD, Goodram, HyperX;
+}
---
+++ Subtotal_Certification_JAVA/TaskWithComparison/src/TaskWithComparison/ComputerPrinting.java
@@ -0,0 +1,11 @@
+package TaskWithComparison;
+
+import java.util.ArrayList;
+
+public class ComputerPrinting {
+
+    public static void MyPrinting(ArrayList<Notebooks> list) {
+        list.forEach(n -> n.about());
+        System.out.println("\n");
+    }
+}
